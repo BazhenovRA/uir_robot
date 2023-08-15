4624af4a8ffdcddd34be6eb1d4ba370439ecb967
BazhenovRA <131593305+BazhenovRA@users.noreply.github.com>
2023-08-15 22:44:26 +0500

    adding sliders and input fields. However, while these are just buttons, they do not work

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from math import acos, asin, sin, cos, sqrt, degrees
-from typing import List, Tuple
+from math import acos, sin, cos, sqrt
 
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
-import matplotlib.widgets as mwidgets
+from matplotlib.widgets import Slider, Button, TextBox
 
 
 def get_path_points(start_point, end_point, discretization):
@@ -77,35 +76,28 @@ def filter_angles(all_angles):
     return filtered_angles_1, filtered_angles_2
 
 
+def calculate_points(path, l_1):
+    return [(l_1 * cos(point[0]), l_1 * sin(point[0])) for point in path]
+
+
 def get_middle_points(path_1, path_2, l_1):
-    middle_points_1 = []
-    middle_points_2 = []
-    for i in range(len(path_1)):
-        middle_point_x_1 = l_1 * cos(path_1[i][0])
-        middle_point_y_1 = l_1 * sin(path_1[i][0])
-        middle_points_1.append((middle_point_x_1, middle_point_y_1))
-    for i in range(len(path_2)):
-        middle_point_x_2 = l_1 * cos(path_2[i][0])
-        middle_point_y_2 = l_1 * sin(path_2[i][0])
-        middle_points_2.append((middle_point_x_2, middle_point_y_2))
+    middle_points_1 = calculate_points(path_1, l_1)
+    middle_points_2 = calculate_points(path_2, l_1)
+
     return middle_points_1, middle_points_2
 
 
 def make_lines(path_points, middle_points_1, middle_points_2):
-    line_1 = [((0, 0), (middle_points_1[i][0], middle_points_1[i][1]), (path_points[i][0], path_points[i][1])) for i in
-              range(len(path_points) - 1)]
-    line_2 = [((0, 0), (middle_points_2[i][0], middle_points_2[i][1]), (path_points[i][0], path_points[i][1])) for i in
-              range(len(path_points) - 1)]
+    # TODO: (0, 0) - robot position (нужно добавить в параметры)
+    line_1 = [((0, 0), middle_points_1[i], path_points[i]) for i in range(len(path_points))]
+    line_2 = [((0, 0), middle_points_2[i], path_points[i]) for i in range(len(path_points))]
 
     return line_1, line_2
 
 
-def show_plot(frames: int, ax, line_1, line_2, ):
-    x_points_1 = [point[0] for point in line_1[frames - 1]]
-    y_points_1 = [point[1] for point in line_1[frames - 1]]
-
-    x_points_2 = [point[0] for point in line_2[frames - 1]]
-    y_points_2 = [point[1] for point in line_2[frames - 1]]
+def show_plot(frames: int, ax, line_1, line_2):
+    x_points_1, y_points_1 = zip(*line_1[frames - 1])
+    x_points_2, y_points_2 = zip(*line_2[frames - 1])
 
     a, = ax.plot(x_points_1, y_points_1, color='green', marker='o', markersize=7)
     b, = ax.plot(x_points_2, y_points_2, color='red', marker='o', markersize=7)
@@ -113,32 +105,73 @@ def show_plot(frames: int, ax, line_1, line_2, ):
     return [a, b]
 
 
-def main():
-    l_1, l_2 = map(float, input('L1 L2: ').split())
-    points1 = tuple(map(float, input(f'X1 Y1: ').split()))
-    points2 = tuple(map(float, input(f'X2 Y2: ').split()))
-    discretization = 100
-    path_points = get_path_points(points1, points2, discretization)
+
+def update_speed(speed_slider):
+    speed = speed_slider.val
+    ani.event_source.interval = speed
+
+
+def update_discretization(l_1, l_2, start_point, end_point, discretization) -> None:
+
+
+    global path_points, all_angles, filtered_angles_1, filtered_angles_2, middle_points_1, middle_points_2, line_1, line_2, path_1, path_2
+    path_points = get_path_points(start_point, end_point, discretization)
     all_angles = get_angles(path_points, l_1, l_2)
     path_1, path_2 = filter_angles(all_angles)
-
-    middle_points_1, middle_points_2 = get_middle_points(path_1, path_2, l_1)
-
+    filtered_angles_1, filtered_angles_2 = filter_angles(all_angles)
+    middle_points_1, middle_points_2 = get_middle_points(filtered_angles_1, filtered_angles_2, l_1)
     line_1, line_2 = make_lines(path_points, middle_points_1, middle_points_2)
 
+    global ani
     fig, ax = plt.subplots()
     ax.set_xlim([-1, 15])
     ax.set_ylim([-1, 15])
 
-    animation = FuncAnimation(fig, show_plot, frames=len(line_1), fargs=(ax, line_1, line_2),
-                              interval=50, blit=True, repeat=True, repeat_delay=300)
     plt.title("Манипулятор на плоскости")
     plt.xlabel('Ось Х')
     plt.ylabel('Ось У')
     plt.legend(['line_1', ' line_2'])
     plt.grid(which='major')
+
+
+    ani = FuncAnimation(fig, show_plot, frames=len(path_points), fargs=(ax, line_1, line_2),
+                        interval=50, blit=True, repeat=True, repeat_delay=300)
+
+    plt.subplots_adjust(bottom=0.35)
+
+
+
+
+    speed_slider_ax = plt.axes([0.2, 0.20, 0.65, 0.03])
+    speed_slider = Slider(speed_slider_ax, 'Speed', 1, 100, valinit=50)
+    speed_slider.on_changed(update_speed)
+
+    discretization_slider_ax = plt.axes([0.2, 0.15, 0.65, 0.03])
+    discretization_slider = Slider(discretization_slider_ax, 'Discretization', 1, 100, valinit=100)
+
+    discretization_slider.on_changed(update_discretization)
+
+    start_x_textbox_ax = plt.axes([0.35, 0.08, 0.1, 0.04])
+    start_x_textbox = TextBox(start_x_textbox_ax, 'start_x ', initial='0')
+    start_y_textbox_ax = plt.axes([0.35, 0.02, 0.1, 0.04])
+    start_y_textbox = TextBox(start_y_textbox_ax, 'start_y ', initial='0')
+    end_x_textbox_ax = plt.axes([0.65, 0.08, 0.1, 0.04])
+    end_x_textbox = TextBox(end_x_textbox_ax, 'end_x ', initial='5')
+    end_y_textbox_ax = plt.axes([0.65, 0.02, 0.1, 0.04])
+    end_y_textbox = TextBox(end_y_textbox_ax, 'end_y ', initial='4')
+
+
     plt.show()
 
 
+
+
 if __name__ == '__main__':
-    main()
+    l_1, l_2 = map(float, input('L1 L2: ').split())
+    start_point = tuple(map(float, input(f'X1 Y1: ').split()))
+    end_point = tuple(map(float, input(f'X2 Y2: ').split()))
+    discretization = 100
+    update_discretization(l_1, l_2, start_point, end_point, discretization)
+
+
+
