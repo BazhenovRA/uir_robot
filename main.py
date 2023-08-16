695507ded4724a6bd171f2a69074fc6a0466380d
RPDavlyatshin <romadavly@gmail.com>
2023-08-16 23:02:50 +0500

    Update main function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import Slider, Button, TextBox
 
+# Global variable for the animation
+ani = None
 
 def get_path_points(start_point, end_point, discretization):
     path_points = []
@@ -50,91 +52,40 @@ def get_middle_points(path_1, path_2, l_1):
 
     return middle_points_1, middle_points_2
 
+def main():
+    # Input parameters
+    l_1, l_2 = map(float, input('L1 L2: ').split())
+    start_point = tuple(map(float, input('X1 Y1: ').split()))
+    end_point = tuple(map(float, input('X2 Y2: ').split()))
 
-def make_lines(path_points, middle_points_1, middle_points_2):
-    line_1 = [((0, 0), middle_points_1[i], path_points[i]) for i in range(len(path_points))]
-    line_2 = [((0, 0), middle_points_2[i], path_points[i]) for i in range(len(path_points))]
-
-    return line_1, line_2
-
-
-def show_plot(frames: int, ax, line_1, line_2):
-    x_points_1, y_points_1 = zip(*line_1[frames - 1])
-    x_points_2, y_points_2 = zip(*line_2[frames - 1])
-
-    a, = ax.plot(x_points_1, y_points_1, color='green', marker='o', markersize=7)
-    b, = ax.plot(x_points_2, y_points_2, color='red', marker='o', markersize=7)
-
-    return [a, b]
-
-
-
-def update_speed(speed_slider):
-    speed = speed_slider.val
-    ani.event_source.interval = speed
-
-
-def update_discretization(l_1, l_2, start_point, end_point, discretization) -> None:
-
-
-    global path_points, all_angles, filtered_angles_1, filtered_angles_2, middle_points_1, middle_points_2, line_1, line_2, path_1, path_2
-    path_points = get_path_points(start_point, end_point, discretization)
-    all_angles = get_angles(path_points, l_1, l_2)
-    path_1, path_2 = filter_angles(all_angles)
-    filtered_angles_1, filtered_angles_2 = filter_angles(all_angles)
-    middle_points_1, middle_points_2 = get_middle_points(filtered_angles_1, filtered_angles_2, l_1)
-    line_1, line_2 = make_lines(path_points, middle_points_1, middle_points_2)
-
-    global ani
+    # Create plot
     fig, ax = plt.subplots()
-    ax.set_xlim([-1, 15])
-    ax.set_ylim([-1, 15])
+    ax.set_xlim([-l_1 - l_2, l_1 + l_2])
+    ax.set_ylim([-l_1 - l_2, l_1 + l_2])
+    ax.set_title("2D Planar Manipulator")
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.grid(True)
 
-    plt.title("Манипулятор на плоскости")
-    plt.xlabel('Ось Х')
-    plt.ylabel('Ось У')
-    plt.legend(['line_1', ' line_2'])
-    plt.grid(which='major')
+    # Create lines for the manipulator's links
+    lines = [ax.plot([], [], 'o-')[0] for _ in range(2)]
 
-
-    ani = FuncAnimation(fig, show_plot, frames=len(path_points), fargs=(ax, line_1, line_2),
-                        interval=50, blit=True, repeat=True, repeat_delay=300)
-
-    plt.subplots_adjust(bottom=0.35)
-
-
-
-
-    speed_slider_ax = plt.axes([0.2, 0.20, 0.65, 0.03])
-    speed_slider = Slider(speed_slider_ax, 'Speed', 1, 100, valinit=50)
-    speed_slider.on_changed(update_speed)
-
-    discretization_slider_ax = plt.axes([0.2, 0.15, 0.65, 0.03])
-    discretization_slider = Slider(discretization_slider_ax, 'Discretization', 1, 100, valinit=100)
+    # Create path points
+    path_points = get_path_points(start_point, end_point, 1000)
 
     discretization_slider.on_changed(update_discretization)
 
-    start_x_textbox_ax = plt.axes([0.35, 0.08, 0.1, 0.04])
-    start_x_textbox = TextBox(start_x_textbox_ax, 'start_x ', initial='0')
-    start_y_textbox_ax = plt.axes([0.35, 0.02, 0.1, 0.04])
-    start_y_textbox = TextBox(start_y_textbox_ax, 'start_y ', initial='0')
-    end_x_textbox_ax = plt.axes([0.65, 0.08, 0.1, 0.04])
-    end_x_textbox = TextBox(end_x_textbox_ax, 'end_x ', initial='5')
-    end_y_textbox_ax = plt.axes([0.65, 0.02, 0.1, 0.04])
-    end_y_textbox = TextBox(end_y_textbox_ax, 'end_y ', initial='4')
+    # Animation
+    global ani
+    ani = FuncAnimation(fig, update_plot,
+                        frames=len(path_points),
+                        interval=1000 / slider.val,
+                        fargs=(path_points, l_1, l_2, lines),
+                        blit=True)
 
-
+    # Show plot
     plt.show()
 
 
-
-
 if __name__ == '__main__':
-    l_1, l_2 = map(float, input('L1 L2: ').split())
-    start_point = tuple(map(float, input(f'X1 Y1: ').split()))
-    end_point = tuple(map(float, input(f'X2 Y2: ').split()))
-    discretization = 100
-    update_discretization(l_1, l_2, start_point, end_point, discretization)
-
-
-
+    main()
